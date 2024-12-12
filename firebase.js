c0e8e84ce8cc32481e69298e2e4e0685986515a5
Solodkyi Serhii <109215037+Serhii1515@users.noreply.github.com>
2024-12-12 09:15:16 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPaC4Ypj2s0XGOFvu6YlQBx1_o6jKf3sE",
+  authDomain: "socialmedia-ec58b.firebaseapp.com",
+  projectId: "socialmedia-ec58b",
+  storageBucket: "socialmedia-ec58b.appspot.com",
+  messagingSenderId: "293759191684",
+  appId: "1:293759191684:web:37b4d57c7c6216d49110d1",
+  measurementId: "G-4JTCM5F8Z2",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
